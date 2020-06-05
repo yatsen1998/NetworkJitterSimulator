24b90c9f63191c678b9285b45edd4d238b551d75
Evan Xu <22747922+yatsen1998@users.noreply.github.com>
2020-06-05 10:22:45 +0800

    add comment and log

--- log_loader.py
+++ log_loader.py
@@ -2,13 +2,15 @@
 import logging
 
 class logInit:
-    def __init__():
-        
+    log_format = "%(asctime)s [%(levelname)s] %(filename)s:%(lineno)d:%(funcName)s(): %(message)s"
     
-    def print_time(self):
+    def __init__():
         logging.basicConfig(format='%(asctime)s - %(levelname)s: %(message)s',
                             filename='NetworkJitter.log',
                             filemode='w',
                             level=logging.INFO)
+    
+    def print_time(self):
+        pass
         
-        print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),'')
+        
--- network_jitter_simulator.py
+++ network_jitter_simulator.py
@@ -54,6 +54,7 @@ class RemoteLink:
         """
         # self.print_time()
         #logging.info(self.hostname + " Set Delay Packet" + '\n')
+        print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
         print(self.hostname + " Set Delay Packet to "+ self.ex_NIC)
         
         stdin, stdout, stderr = ssh.exec_command("sudo tc qdisc add dev " + 
@@ -83,6 +84,10 @@ class RemoteLink:
                                                  " root netem loss 20%")
 
     def test_set_loss_in(self,ssh):
+        """
+            Testcase4: set packet loss to internal NIC
+
+        """
         print(self.hostname + " Set Packet Loss to "+ self.in_NIC)
         
         stdin, stdout, stderr = ssh.exec_command("sudo tc qdisc add dev " + 
@@ -90,6 +95,10 @@ class RemoteLink:
                                                  " root netem loss 20%")
     
     def test_set_dup_ex(self,ssh):
+        """
+            Testcase5: set duplicate packet to external NIC
+
+        """        
         print(self.hostname + " Set Duplicate Packet to "+ self.ex_NIC)
         
         stdin, stdout, stderr = ssh.exec_command("sudo tc qdisc add dev " + 
@@ -97,6 +106,10 @@ class RemoteLink:
                                                  " root netem duplicate 20%")
     
     def test_set_dup_in(self,ssh):
+        """
+            Testcase6: set duplicate packet to internal NIC
+
+        """
         print(self.hostname + " Set Duplicate Packet to "+ self.in_NIC)
         
         stdin, stdout, stderr = ssh.exec_command("sudo tc qdisc add dev " + 
@@ -104,6 +117,10 @@ class RemoteLink:
                                                  " root netem duplicate 20%")
     
     def test_set_corrupt_ex(self,ssh):
+        """
+            Testcase7: set corrupt packet to external NIC
+
+        """
         print(self.hostname + " Set Corrupt Packet to "+ self.ex_NIC)
         
         stdin, stdout, stderr = ssh.exec_command("sudo tc qdisc add dev " + 
@@ -111,6 +128,10 @@ class RemoteLink:
                                                  " root netem corrupt 20%")
     
     def test_set_corrupt_in(self,ssh):
+        """
+            Testcase8: set corrupt packet to internal NIC
+
+        """
         print(self.hostname + " Set Corrupt Packet to "+ self.in_NIC)
         
         stdin, stdout, stderr = ssh.exec_command("sudo tc qdisc add dev " + 
@@ -118,6 +139,10 @@ class RemoteLink:
                                                  " root netem corrupt 20%")
     
     def test_set_scrambled_ex(self,ssh):
+        """
+            Testcase9: set scrambled packet to external NIC
+
+        """
         print(self.hostname + " Set Scrambled Packet to "+ self.ex_NIC)
         
         stdin, stdout, stderr = ssh.exec_command("sudo tc qdisc add dev " + 
@@ -125,6 +150,10 @@ class RemoteLink:
                                                  " root netem reorder 20% 20%")
     
     def test_set_scrambled_in(self,ssh):
+        """
+            Testcase10: set scrambled packet to internal NIC
+
+        """       
         print(self.hostname + " Set Scrambled Packet to "+ self.in_NIC)
         
         stdin, stdout, stderr = ssh.exec_command("sudo tc qdisc add dev " + 
@@ -132,18 +161,28 @@ class RemoteLink:
                                                  " root netem reorder 20% 20%")
     
     def clean_ex(self,ssh):
+        """
+            Clean rules set to external NIC
+
+        """
         print(self.hostname + "Clean command "+ self.ex_NIC)
         stdin, stdout, stderr = ssh.exec_command("sudo tc qdisc del dev " + 
                                                  self.ex_NIC + " root")
     
     def clean_in(self,ssh):
+        """
+            Clean Rules set to internal NIC
+
+        """
         print(self.hostname + "Clean command "+ self.in_NIC)
         stdin, stdout, stderr = ssh.exec_command("sudo tc qdisc del dev " + 
                                                  self.in_NIC + " root")
         
 
     def test_set_mixed_jitter(self, ssh, interval, elapse_time):
-        
+        """
+            Testcase11: set a network condition to certain NIC
+        """
         timeout = time.time() + elapse_time
         Rule_map = ["test_set_delay_ex","test_set_delay_in",
                     "test_set_loss_ex","test_set_loss_in",
